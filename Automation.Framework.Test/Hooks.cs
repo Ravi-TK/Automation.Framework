ede3491a387e85a79a90a164d49d0931547c539b
Ravi Kota <ravi.kota@wescot.co.uk>
2020-03-11 20:35:40 +0000

    Keyboard actions test

--- Automation.Framework.Test/Hooks.cs
+++ Automation.Framework.Test/Hooks.cs
@@ -3,6 +3,7 @@ using Automation.Framework.Core;
 using Automation.Framework.Test.Pages;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
 using Unity;
 using Unity.Lifetime;
 
@@ -17,6 +18,17 @@ namespace Automation.Framework.Test
             Driver.StartBrowser(BrowserTypes.Chrome);
             UnityContainerFactory.GetContainer().RegisterType<W3schoolPage>(new ContainerControlledLifetimeManager());
             UnityContainerFactory.GetContainer().RegisterInstance<IWebDriver>(Driver.Browser);
+        }
+
+        private static InternetExplorerOptions GetIEOptions()
+        {
+            InternetExplorerOptions options = new InternetExplorerOptions();
+            options.IntroduceInstabilityByIgnoringProtectedModeSettings = true;
+            options.IgnoreZoomLevel = true;
+            options.EnableNativeEvents = true;
+            options.EnsureCleanSession = true;
+            options.ElementScrollBehavior = InternetExplorerElementScrollBehavior.Bottom;
+            return options;
         }
 
         [TestInitialize]
--- Automation.Framework.Test/Tests/KeyBoardActionTest.cs
+++ Automation.Framework.Test/Tests/KeyBoardActionTest.cs
@@ -1,7 +1,9 @@
 ﻿using Automation.Framework.Base;
+using Automation.Framework.Core;
 using Automation.Framework.Test.Pages;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
 using Unity;
 
 namespace Automation.Framework.Test.Tests
@@ -17,7 +19,21 @@ namespace Automation.Framework.Test.Tests
             _KeyBoardActionPage.Helper.BrowserHelper.Navigate("https://www.w3schools.com/");
             _KeyBoardActionPage.Helper.BrowserHelper.BrowserMaximise();
 
-            _KeyBoardActionPage.Helper.KeyBoardActionHelper.SingleCommandKeyAction(Keys.LeftControl, "t");
+           // string selectall= Keys.
+            //Driver.Browser.FindElement
+            //    (By.XPath("/html/body/div[1]/div[2]/div[@class='w3-right']"))
+            //    .SendKeys(Keys.Control + "a");
+
+            Actions act = new Actions(Driver.Browser);
+
+            act.KeyDown(Keys.Control)
+                .SendKeys("T")
+                .KeyUp(Keys.Control)
+                .Build()
+                .Perform();
+
+
+            //_KeyBoardActionPage.Helper.KeyBoardActionHelper.SingleCommandKeyAction(Keys.LeftControl, "t");
             _KeyBoardActionPage.Helper.BrowserHelper.SwitchToWindow(1);
         }
 
